6e21990acc4497344f326cfdbebb63af64445c68
Della Bella <gisdellabella@uol.com.br>
2025-04-01 10:15:11 +0100

    fisrt JS codes

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-alert: "hello!"
-conslelog.alert
+
+cont apiKey= 9fc35e3c8a4706fad4c9af100dfa8bd8
+constapiUrl= https://api.openweathermap.org/data/2.5/weather?units=metricq&=london";
+
+async function checkWeather(){
+    const response= await fetch ( apiUrl + `&appid=${apiKey}`);
+    var data= awiat response.jason();
+
+    console.log(data);
+}
+checkWeather()
